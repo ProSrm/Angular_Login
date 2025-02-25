3628a8c7f550fc24fcd64c51653eeaff2e100425
Shubham Mate <shubham.r.mate@softmass.co.in>
2025-02-25 16:46:17 +0530

    Initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true, 
+  imports: [ReactiveFormsModule, CommonModule], 
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const loginData = {
+        username: this.loginForm.value.username,
+        password: this.loginForm.value.password
+      };
+
+      this.http.post('https://localhost:7067/api/layer/login', loginData)
+        .subscribe({
+          next: (response) => {
+            console.log('Login successful', response);
+            this.router.navigate(['/dashboard']);
+          },
+          error: (error) => {
+            console.error('Login failed', error);
+          }
+        });
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
